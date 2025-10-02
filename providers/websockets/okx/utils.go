d5a87d19df3e3376b0dd6c5b38d7f4878ede0a16
Matthew Weeks <matthew.h.weeks@gmail.com>
2025-10-02 13:32:47 -0400

    chore(okx): remove unused URL's

--- providers/websockets/okx/utils.go
+++ providers/websockets/okx/utils.go
@@ -7,19 +7,9 @@ import (
 )
 
 const (
-	// OKX provides a few different URLs for its Websocket API. The URLs can be found
-	// in the documentation here: https://www.okx.com/docs-v5/en/?shell#overview-production-trading-services
-	// The two production URLs are defined in ProductionURL and ProductionAWSURL. The
-	// DemoURL is used for testing purposes.
 
-	// Name is the name of the OKX provider.
 	Name = "okx_ws"
-
-	// URL_PROD is the public OKX Websocket URL.
-	URL_PROD = "wss://ws.okx.com:8443/ws/v5/public"
-
-	// URL_DEMO is the public OKX Websocket URL for test usage.
-	URL_DEMO = "wss://wspap.okx.com:8443/ws/v5/public?brokerId=9999"
+	URL = "wss://ws.okx.com:8443/ws/v5/public"
 
 	// WriteInterval is the interval at which the OKX Websocket will write to the connection.
 	// By default, there can be 3 messages written to the connection every second. Or 480
@@ -49,7 +39,7 @@ var DefaultWebSocketConfig = config.WebSocketConfig{
 	MaxBufferSize:                 config.DefaultMaxBufferSize,
 	ReconnectionTimeout:           config.DefaultReconnectionTimeout,
 	PostConnectionTimeout:         config.DefaultPostConnectionTimeout,
-	Endpoints:                     []config.Endpoint{{URL: URL_PROD}},
+	Endpoints:                     []config.Endpoint{{URL: URL}},
 	ReadBufferSize:                config.DefaultReadBufferSize,
 	WriteBufferSize:               config.DefaultWriteBufferSize,
 	HandshakeTimeout:              config.DefaultHandshakeTimeout,
